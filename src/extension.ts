3488c8581b39f6954c26efb0c136b2d7db277a76
Ortmann, Florian <florian.ortmann@axenton.com>
2019-12-10 16:31:02 +0100

    Prettified files, ignored .vscode/settings.json

--- src/extension.ts
+++ src/extension.ts
@@ -5,17 +5,18 @@ import { createSchemaCommand } from './commands/SchemaInputCommand';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log(
+        'Congratulations, your extension "graphql-client-helper" is now active!'
+    );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "graphql-client-helper" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	
-	// Register commands here -> commands can be found in the /commands directory
-	context.subscriptions.push(createSchemaCommand);
+    // Register commands here -> commands can be found in the /commands directory
+    context.subscriptions.push(createSchemaCommand);
 }
 
 // this method is called when your extension is deactivated
--- src/test/suite/util.test.ts
+++ src/test/suite/util.test.ts
@@ -7,14 +7,13 @@ import { isValidURL } from '../../Utils/Utils';
 import GraphQLUtils from '../../Utils/GraphQLUtils';
 import { expect } from 'chai';
 
-
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
 // import * as myExtension from '../extension';
 
 describe('Util tests', function() {
-	vscode.window.showInformationMessage('Start all tests.');
-	describe('isValidURLTest to test if url is validated', () => {
+    vscode.window.showInformationMessage('Start all tests.');
+    describe('isValidURLTest to test if url is validated', () => {
         it('should return true', () => {
             expect(isValidURL('http://google.com')).to.equal(true);
         });
@@ -29,20 +28,22 @@ describe('Util tests', function() {
 
         it('should return false', () => {
             expect(isValidURL('google.com')).to.equal(false);
-        });      
+        });
     });
-    
+
     describe('GraphQLUtilTest to test if invalid endpoint throws error', () => {
-        let url:string = 'http://google.com';
-        let current_dir:string = path.join(__dirname, 'graphqlschema');
+        let url: string = 'http://google.com';
+        let current_dir: string = path.join(__dirname, 'graphqlschema');
         let util = new GraphQLUtils(__dirname);
         it('should return true since the folder is created after creating instance of GraphQLUtils', () => {
             expect(fs.existsSync(current_dir)).to.equal(true);
         });
-        it('should throw an error, response does not contain a body', async () => {       
+        it('should throw an error, response does not contain a body', async () => {
             this.timeout(0);
-            await util.getSchemaFromEndpoint(url).catch((err) => expect(err).to.be.instanceOf(Error));
-        });     
+            await util
+                .getSchemaFromEndpoint(url)
+                .catch(err => expect(err).to.be.instanceOf(Error));
+        });
         it('should return false, since the folder gets deleted if error is thrown', () => {
             expect(fs.existsSync(current_dir)).to.equal(false);
         });
