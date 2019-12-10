a1b24f9282d7b0eddde097ba4b2cf71ac12af5a6
Ortmann, Florian <florian.ortmann@axenton.com>
2019-12-10 14:24:59 +0100

    Updated structure to organize commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import GraphQLUtils from './Utils/GraphQLUtils';
-import { isValidURL } from './Utils/Utils';
-
+import { createSchemaCommand } from './commands/SchemaInputCommand';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -15,27 +13,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-
-	// Runs an introspection query on given endpoint and creates a schema file
-	let createSchema = vscode.commands.registerCommand('extension.createSchema', () => {
-		vscode.window.showInputBox().then((value) => {
-			if(value !== undefined && isValidURL(value)){	
-				if(vscode.workspace.workspaceFolders !== undefined){
-					let pathToFolder = vscode.workspace.workspaceFolders[0];
-					let util = new GraphQLUtils(pathToFolder.uri.fsPath);
-					let schema = util.getSchemaFromEndpoint(value)
-						.catch(error => vscode.window.showErrorMessage('Error creating Schema:'+ error));								
-					
-				}else{
-					vscode.window.showErrorMessage("No workspace opened!");
-				}
-			}else{
-				vscode.window.showErrorMessage('Invalid path provided!');
-			}
-		});
-	});
-
-	context.subscriptions.push(createSchema);
+	
+	// Register commands here -> commands can be found in the /commands directory
+	context.subscriptions.push(createSchemaCommand);
 }
 
 // this method is called when your extension is deactivated
