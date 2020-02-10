caee90460a271423c8d820a0a5584b04120a42d3
Ortmann, Florian <florian.ortmann@axenton.com>
2020-02-10 13:59:50 +0100

    Created CircularQuickInputProvider

--- src/commands/ExecuteRequestCommand.ts
+++ src/commands/ExecuteRequestCommand.ts
@@ -1,7 +1,7 @@
 import { CustomRequest } from '../provider/SavedRequestNodeProvider';
 import { ClientService } from '../services/ClientService';
 import * as vscode from 'vscode';
-import { FieldWrapper } from '../graphqlwrapper/FieldWrapper';
+import { CircularQuickInput } from '../provider/CircularQuickInputProvider';
 
 /**
  * Executes a request with the internal graphqlclient,
@@ -15,96 +15,16 @@ export async function executeRequestCommand(
     channel: vscode.OutputChannel
 ) {
     channel.clear();
-    let vars = '{';
-    const steps = node.args.length;
-    var count = 0;
-    while (count < steps) {
-        if (count !== 0) {
-            vars += ',';
-        }
-        let arg = node.args[count];
-        let value = await vscode.window.showInputBox({
-            prompt: arg.nonNull
-                ? 'Please provide a value for the required argument' +
-                  arg.name +
-                  '!'
-                : 'Please provide a value for the optional argument ' +
-                  arg.name,
-            validateInput: text => {
-                return validateType(arg, text)
-                    ? null
-                    : 'Invalid type for argument of type: ' + arg.ofType;
-            }
-        });
-
-        if (value === undefined || value === '') {
-            value = 'null';
-        }
-        vars = vars += argToString(arg, value);
-        count++;
-    }
-    vars = vars += '}';
+    const quickInput = new CircularQuickInput(node.label, node.args);
+    let vars = await quickInput.show();
     client
         .executeRequest(node.request, JSON.parse(vars))
         .then(data => {
             channel.appendLine(data);
             channel.show();
         })
-        .catch(error => vscode.window.showErrorMessage(error));
-}
-
-/**
- * Validates the inputvalue is of the right type
- * @param arg Argument
- * @param value Inputvalue for the argument
- */
-function validateType(arg: FieldWrapper, value: string): boolean {
-    if (value === undefined || value === '') {
-        if (arg.nonNull === false) {
-            return true;
-        }
-        return false;
-    }
-    if (arg.ofType === 'Boolean') {
-        if (value.match(/^true|false/)) {
-            return true;
-        } else {
-            return false;
-        }
-    } else if (arg.ofType === 'Int') {
-        if (Number.parseInt(value) !== undefined) {
-            return true;
-        } else {
-            return false;
-        }
-    } else if (arg.ofType === 'Float') {
-        if (Number.parseFloat(value) !== undefined) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    if (arg.nonNull) {
-        return false;
-    }
-    return true;
-}
-
-/**
- * Wraps an argument with its input as string
- * @param arg Argument
- * @param value Inputvalue
- */
-function argToString(arg: FieldWrapper, value: string): string {
-    if (value === 'null') {
-        return `"${arg.name}": null`;
-    }
-    if (arg.ofType === 'Boolean') {
-        return `"${arg.name}": ${value}`;
-    } else if (arg.ofType === 'Int') {
-        return `"${arg.name}": ${Number.parseInt(value)}`;
-    } else if (arg.ofType === 'Float') {
-        return `"${arg.name}": ${Number.parseFloat(value)}`;
-    }
-    return `"${arg.name}": "${value}"`;
+        .catch(error => {
+            var obj = JSON.parse(error);
+            vscode.window.showErrorMessage(obj.response.errors[0].message);
+        });
 }
---
+++ src/provider/CircularQuickInputProvider.ts
@@ -0,0 +1,250 @@
+import { FieldWrapper } from '../graphqlwrapper/FieldWrapper';
+import * as vscode from 'vscode';
+export class CircularQuickInput {
+    private _argumentGroup: ArgumentItem[];
+    private _quickPick: vscode.QuickPick<ArgumentItem>;
+    constructor(private _titel, private args: FieldWrapper[]) {
+        this._argumentGroup = args.map(
+            arg =>
+                new ArgumentItem(
+                    arg.name,
+                    '',
+                    arg.ofType,
+                    arg.nonNull,
+                    arg.description
+                )
+        );
+
+        this._quickPick = vscode.window.createQuickPick();
+        this._quickPick.items = this._argumentGroup;
+        this._quickPick.buttons = [];
+        this._quickPick.placeholder = `Please select an argument:`;
+        this._quickPick.canSelectMany = false;
+        this._quickPick.title = this._titel;
+        this._quickPick.buttons = [new FilterArgumentButton('', 'Filter')];
+        this._quickPick.onDidTriggerButton(button => {
+            if (this._quickPick !== undefined) {
+                this._quickPick.items = this._argumentGroup.filter(
+                    item => item.nonNull === true
+                );
+            }
+        });
+    }
+
+    createQuickPick(): vscode.QuickPick<ArgumentItem> {
+        const input = vscode.window.createQuickPick<ArgumentItem>();
+        input.items = this._argumentGroup.sort((x, y) => {
+            return x.isSet === y.isSet ? 0 : x.isSet ? 1 : -1;
+        });
+        input.placeholder = `Please select an argument:`;
+        input.canSelectMany = false;
+        input.title = this._titel;
+        input.buttons = [new FilterArgumentButton('', 'Filter')];
+        return input;
+    }
+
+    async showQuickPick(): Promise<ArgumentItem> {
+        const disposables: vscode.Disposable[] = [];
+        try {
+            return await new Promise<ArgumentItem>((resolve, reject) => {
+                if (this._quickPick) {
+                    this._quickPick.dispose();
+                }
+                this._quickPick = this.createQuickPick();
+
+                disposables.push(
+                    this._quickPick.onDidChangeSelection(items =>
+                        resolve(items[0])
+                    ),
+                    this._quickPick.onDidTriggerButton(button => {
+                        if (this._quickPick !== undefined) {
+                            this._quickPick.items.length ===
+                            this._argumentGroup.length
+                                ? (this._quickPick.items = this._argumentGroup.filter(
+                                      item => item.nonNull === true
+                                  ))
+                                : (this._quickPick.items = this._argumentGroup);
+                        }
+                    })
+                );
+                this._quickPick.show();
+            });
+        } finally {
+            disposables.forEach(item => item.dispose());
+        }
+    }
+
+    async show(): Promise<string> {
+        let item = await this.showQuickPick();
+        if (item !== undefined) {
+            return new Promise<string>(async (resolve, reject) => {
+                await vscode.window
+                    .showInputBox({
+                        placeHolder: item.value,
+                        prompt: item.nonNull
+                            ? 'Please provide a value for the required argument ' +
+                              item.name +
+                              '!'
+                            : 'Please provide a value for the optional argument ' +
+                              item.name,
+                        validateInput: text => {
+                            return this.validateType(item, text)
+                                ? null
+                                : 'Invalid type for argument of type: ' +
+                                      item.ofType;
+                        }
+                    })
+                    .then(async value => {
+                        if (value === '' || item.nonNull === false) {
+                            value = 'null';
+                        }
+                        if (value !== undefined) {
+                            item.value = value;
+                        } else {
+                            resolve(this.show());
+                        }
+                        if (
+                            this._argumentGroup.filter(
+                                item => item.isSet !== true
+                            ).length > 0
+                        ) {
+                            resolve(this.show());
+                        } else {
+                            const args =
+                                '{' +
+                                this._argumentGroup
+                                    .map(item => this.argToString(item))
+                                    .join(', ') +
+                                '}';
+                            await vscode.window
+                                .showInformationMessage(
+                                    `Do you want to run request with arguments: ${args}?`,
+                                    'Yes',
+                                    'No'
+                                )
+                                .then(button => {
+                                    if (button === 'No') {
+                                        resolve(this.show());
+                                    }
+                                });
+                            resolve(args);
+                        }
+                    });
+            });
+        } else {
+            return Promise.reject();
+        }
+    }
+
+    /**
+     * Validates the inputvalue is of the right type
+     * @param arg Argument
+     * @param value Inputvalue for the argument
+     */
+    validateType(arg: ArgumentItem, value: string): boolean {
+        if (value === undefined || value === '') {
+            if (arg.nonNull === false) {
+                return true;
+            }
+            return false;
+        }
+        if (arg.ofType === 'Boolean') {
+            if (value.match(/^true|false/)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (arg.ofType === 'Int') {
+            var numb = Number.parseInt(value);
+            if (!isNaN(numb)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else if (arg.ofType === 'Float') {
+            var float = Number.parseFloat(value);
+            if (!isNaN(float)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (arg.nonNull) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Wraps an argument with its input as string
+     * @param arg Argument
+     * @param value Inputvalue
+     */
+    argToString(arg: ArgumentItem): string {
+        if (arg.value === 'null') {
+            return `"${arg.name}": null`;
+        }
+        if (arg.ofType === 'Boolean') {
+            return `"${arg.name}": ${arg.value}`;
+        } else if (arg.ofType === 'Int') {
+            return `"${arg.name}": ${Number.parseInt(arg.value)}`;
+        } else if (arg.ofType === 'Float') {
+            return `"${arg.name}": ${Number.parseFloat(arg.value)}`;
+        }
+        return `"${arg.name}": "${arg.value}"`;
+    }
+}
+
+class FilterArgumentButton implements vscode.QuickInputButton {
+    constructor(
+        public iconPath:
+            | vscode.Uri
+            | { light: vscode.Uri; dark: vscode.Uri }
+            | vscode.ThemeIcon,
+        public tooltip?: string | undefined
+    ) {}
+}
+
+class ArgumentItem implements vscode.QuickPickItem {
+    constructor(
+        private _key: string,
+        private _value: string,
+        private _ofType: string,
+        private _nullable: boolean,
+        public description?: string | undefined,
+        detail?: string | undefined,
+        picked?: boolean | undefined,
+        alwaysShow?: boolean | undefined
+    ) {}
+
+    get label(): string {
+        return `${this._key}: ${this._value}`;
+    }
+
+    get isSet(): boolean {
+        if (this._value !== '' && this._value !== undefined) {
+            return true;
+        }
+        return false;
+    }
+
+    get nonNull(): boolean {
+        return this._nullable;
+    }
+
+    get name(): string {
+        return this._key;
+    }
+
+    get ofType(): string {
+        return this._ofType;
+    }
+
+    get value(): string {
+        return this._value;
+    }
+
+    set value(value: string) {
+        this._value = value;
+    }
+}
